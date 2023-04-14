ece4e7970a26549c9fa52c5560f52ce198125518
Batuhan Gülcemal <bthnglcml001@hotmail.com>
2023-04-14 14:15:32 +0300

    Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float rotateSpeed = 7f;
     [SerializeField] private GameInput gameInput;
 
+    private float playerRadius = 0.7f;
+    private float playerHeight = 2f;
+
     private bool isWalking;
     private void Update()
     {
@@ -15,8 +18,37 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDistance * moveDir;
+        }
+
         isWalking = moveDir != Vector3.zero;
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
