ceb37127c6a1dbe5c20499cbad261d2fac703eae
Batuhan Gülcemal <bthnglcml001@hotmail.com>
2023-04-19 14:02:56 +0300

    Delivery Manager

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    public static DeliveryManager Instance { get; private set; }
+
+    [SerializeField] private RecipeListSO recipeListSO;
+
+    private List<RecipeSO> waitingRecipeSOList;
+
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        Instance = this;
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if(spawnRecipeTimer < 0) 
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+            if(waitingRecipesMax > waitingRecipeSOList.Count)
+            {
+                RecipeSO waitingRecipeSo = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSo.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSo);
+
+            }
+
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                //has the same number of ingredients
+                bool plateContentsMatchesRecipe = true;
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    bool ingredientFound = false;
+
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        if(plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            //Ingredient Match
+                            ingredientFound = true;
+                        }
+                    }
+                    
+                    if(!ingredientFound)
+                    {
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipe)
+                {
+                    //Recipe match!
+
+                    waitingRecipeSOList.RemoveAt(i);
+
+                    return;
+                }
+            }
+        }
+
+
+        //No matches
+        return;
+    }
+}
